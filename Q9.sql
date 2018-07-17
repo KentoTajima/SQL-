32b8b849f37a514f80453f4e95bf4d315557d75c
tajimakento <tajiken1214@gmail.com>
2018-07-17 14:50:10 +0900

    SQL 問9まで完了しました。

---
+++ Q9.sql
@@ -0,0 +1,9 @@
+SELECT
+category_name,
+SUM(item_price) AS total_price
+FROM
+item INNER JOIN item_category
+ON item.category_id=item_category.category_id
+GROUP BY category_name
+ORDER BY total_price desc
+;
